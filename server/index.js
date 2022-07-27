c2fd14515fae5de5d06c8776dec3f170f90bf91d
Ayesha Sahar <83140034+AyeshaSahar@users.noreply.github.com>
2022-07-27 16:56:48 +0500

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+import userRouter from "./routes/user.js";
+
+import dotenv from 'dotenv';
+const app = express();
+
+dotenv.config();
+app.use(express.json({ limit: '30mb', extended: true }))
+app.use(express.urlencoded({ limit: '30mb', extended: true }))
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use("/user", userRouter);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+const CONNECTION_URL = 'mongodb+srv://Ayesha:a1b2c3d4e5xyz@cluster0.2rt6s.mongodb.net/?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT|| 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set('useFindAndModify', false);
